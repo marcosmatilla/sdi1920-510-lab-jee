19f358ab0074bbd713a20dcacd6aa79e5992068f
UO258935 <UO258935@uniovi.es>
2020-02-04 10:28:21 +0100

    SDI–session1-Ejercicio-complementario1

--- WebContent/vista-productos.jsp
+++ WebContent/vista-productos.jsp
@@ -1,8 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
 	pageEncoding="utf-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
 <title>JSP</title>
@@ -18,18 +17,16 @@
 <body>
 	<!-- Contenido -->
 	<div class="container" id="contenedor-principal">
-		<h2>Vista-Carrito</h2>
+		<h2>Vista-Producto</h2>
 		<ul>
-			<c:forEach var="par" items="${paresCarrito}">
-				<tr>
-					<li>${par.key}- ${par.value}</li>
-				</tr>
+			<c:forEach var="producto" items="${productosTienda}">
+				<div>
+					<c:out value="${producto.nombre}" />
+					<c:out value="${producto.precio}" />
+					€
+				</div>
 			</c:forEach>
 		</ul>
 	</div>
 </body>
 </html>
-
-
-
-
--- src/com/uniovi/sdi/ServletCarrito.java
+++ src/com/uniovi/sdi/ServletCarrito.java
@@ -34,27 +34,26 @@ public class ServletCarrito extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		HttpSession session = request.getSession();
-		
+
 		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito");
-		
+
 		// No hay carrito, creamos uno y lo insertamos en sesión
 		if (carrito == null) {
 			carrito = new HashMap<String, Integer>();
 			request.getSession().setAttribute("carrito", carrito);
 		}
-		
+
 		String producto = request.getParameter("producto");
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
 		}
-		
+
 		// Retornar la vista con parámetro "carrito"
 		request.setAttribute("paresCarrito", carrito);
-		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request,
-		response);
-		
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
+
 //		response.setCharacterEncoding("UTF-8");
 //		response.setContentType("text/html");
 //		
@@ -66,21 +65,20 @@ public class ServletCarrito extends HttpServlet {
 //		out.println(carritoEnHTML(carrito) + "<br>");
 //		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
 	}
-	
+
 	private void insertarEnCarrito(Map<String, Integer> carrito, String claveProducto) {
-		if(carrito.get(claveProducto) ==  null) {
+		if (carrito.get(claveProducto) == null) {
 			carrito.put(claveProducto, new Integer(1));
-		}
-		else {
-			int numeroArticulos=(Integer)carrito.get(claveProducto).intValue();
-			carrito.put(claveProducto, new Integer(numeroArticulos+1));
+		} else {
+			int numeroArticulos = (Integer) carrito.get(claveProducto).intValue();
+			carrito.put(claveProducto, new Integer(numeroArticulos + 1));
 		}
 	}
-	
+
 	private String carritoEnHTML(Map<String, Integer> carrito) {
 		String carritoEnHTML = "";
-		for(String key: carrito.keySet()) {
-			carritoEnHTML+="<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
+		for (String key : carrito.keySet()) {
+			carritoEnHTML += "<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
 		}
 		return carritoEnHTML;
 	}
